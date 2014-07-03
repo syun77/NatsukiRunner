60e2716766487d61cab0d71cb8c10d980417a86c
syun <2dgames.jp@gmail.com>
2014-07-03 22:43:59 +0900

    アンロックウィンドウを開く演出を実装

--- source/DialogUnlock.hx
+++ source/DialogUnlock.hx
@@ -11,6 +11,7 @@ import flixel.group.FlxGroup;
  * アンロックダイアログ
  **/
 class DialogUnlock extends FlxGroup {
+    private static inline var TIMER = 60;
 
     private var _frame:FlxSprite;
     private var _window:FlxSprite;
@@ -18,6 +19,7 @@ class DialogUnlock extends FlxGroup {
     private var _btn:FlxButton;
     private var _objs:Array<FlxObject>;
     private var _bClose:Bool = false; // 閉じたかどうか
+    private var _timer:Float = TIMER;
 
     /**
      * コンストラクタ
@@ -34,15 +36,19 @@ class DialogUnlock extends FlxGroup {
         var y = FlxG.height/2 - h/2;
         _frame = new FlxSprite(x-2, y-2);
         _frame.makeGraphic(w+4, h+4, FlxColor.SILVER);
+        _frame.scale.y = 0;
         _window = new FlxSprite(x, y);
         _window.makeGraphic(w, h, FlxColor.NAVY_BLUE);
+        _window.scale.y = 0;
         _txt = new FlxText(0, y + 8, FlxG.width);
         _txt.alignment = "center";
         _txt.text = "Unlock " + Reg.getLevelName(lv) + " mode.";
+        _txt.visible = false;
         _btn = new FlxButton(FlxG.width/2-40, y + 32, _cbOk);
         _btn.text = "OK";
         _btn.color = FlxColor.AZURE;
         _btn.label.color = FlxColor.AQUAMARINE;
+        _btn.visible = false;
 
         _objs.push(_frame);
         _objs.push(_window);
@@ -53,6 +59,27 @@ class DialogUnlock extends FlxGroup {
             o.scrollFactor.set(0, 0);
             this.add(o);
         }
+    }
+
+    /**
+     * 更新
+     **/
+    override public function update():Void {
+        super.update();
+
+        if(_timer > 1) {
+            _timer = _timer * 0.8;
+            var v = (TIMER - _timer) / TIMER;
+            _frame.scale.y = v;
+            _window.scale.y = v;
+            if(_timer <= 1) {
+                _btn.visible = true;
+                _txt.visible = true;
+                _frame.scale.y = 1;
+                _window.scale.y = 1;
+            }
+        }
+
     }
 
     /**
